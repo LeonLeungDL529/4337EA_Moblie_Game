9e9552f05dca0c890cc43d09f582a8fafdf4c28f
Dickson200055272 <200055272@stu.vtc.edu.hk>
2022-04-13 18:09:04 +0800

    Start_scene

---
+++ Assets/RTS_EA_4332/Script/Menu_Manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {}
+    public void StartGame()
+    {
+
+    }
+
+
+    public void Quit_Game()
+    {
+        Application.Quit();
+    }
+}
